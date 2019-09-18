a190ecb476371a5726f2ab7c7916be3c3b159685
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-18 19:11:06 +0100

    WIP: initial commit

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const Users = require('../users/users-model.js');
+const secrets = require('./../config/secrets')
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -25,9 +27,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
+        const token = generateToken(user)
+        res.status(200).json({ token });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -36,5 +37,15 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, secrets.jwtSecret, options) 
+}
 
 module.exports = router;
